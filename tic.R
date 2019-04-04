97542e0ae10e2ac33122b7cb832ac59db90f4070
alexrecuenco <safale93@gmail.com>
2019-04-05 01:16:08 +0700

    Before_install should always happen

--- tic.R
+++ tic.R
@@ -40,14 +40,15 @@ step_setup_linux_gcc49 <- function() {
 # - if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then echo "VkVSPS00LjkKQ0M9Z2NjJChWRVIpIC1zdGQ9YzExIApDWFg9ZysrJChWRVIpClNITElCX0NYWExEPWcrKyQoVkVSKQpGQz1nZm9ydHJhbgpGNzc9Z2ZvcnRyYW4K" | base64 --decode > ~/.R/Makevars; fi
 # - if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then cat ~/.R/Makevars; fi
 
+get_stage("before_install") %>%
+    add_step(step_setup_linux_gcc49())
+
 
 if (Sys.getenv("id_rsa") != "" && !ci()$is_tag()) {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_install") %>%
-        add_step(step_setup_linux_gcc49())
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
