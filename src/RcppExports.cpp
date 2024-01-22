10d2303d13cf8e285fc3ec6677cd4f9d5f44d112
alexrecuenco <alejandrogonzalezrecuenco@gmail.com>
2024-01-22 23:30:03 +0100

    Remove C++11 as recommended by CRAN

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // CompileDocument
 List CompileDocument(const StringVector& x, const StringVector& layersNames, const StringVector& layersCmd);
 RcppExport SEXP _TexExamRandomizer_CompileDocument(SEXP xSEXP, SEXP layersNamesSEXP, SEXP layersCmdSEXP) {
